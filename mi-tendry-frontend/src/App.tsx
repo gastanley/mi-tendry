0bec1c1ed73638c284369219983fff19188a9a99
tellar <gastonstanley17@gmail.com>
2025-08-19 15:06:27 +0300

    dépendances de base installé

---
+++ mi-tendry-frontend/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+      <main>
+        <h1 className='text-2xl text-blue-300 font-bold'>Vite + React + Typescript + SMC installed successfully</h1>
+      </main>
+  )
+}
+
+export default App
